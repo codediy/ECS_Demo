1d7c441f89450249fb5688372fec19dbeeb97bf8
zmw <zmwwork@163.com>
2019-08-16 10:29:17 +0800

    ECS removeComponent()回调

--- src/js/lib/ecs/entity.js
+++ src/js/lib/ecs/entity.js
@@ -4,6 +4,7 @@ export default class Entity {
 	constructor(id) {
 		this._id = id || (currentId++).toString(36);
 		this._components = new Map();
+		/*属性组件掩码*/
 		this._mask = 0;
 	}
 
@@ -23,18 +24,21 @@ export default class Entity {
 		return this;
 	}
 
+	/*移除属性组件回调 需要注册一个删除回调函数*/
 	removeComponents(...components) {
 		components.forEach((component) => {
 			const entityComponent = this._components.get(component);
-
+			/*属性组件删除回调*/
 			if (entityComponent) {
 				this._components.delete(entityComponent.constructor.id);
+				/*mask掩码变化*/
 				this._mask &= ~entityComponent.constructor.mask;
 			}
 		});
 		return this;
 	}
 
+	/*检查是否包含属性组件*/
 	hasComponent(component) {
 		return this._components.has(component);
 	}
